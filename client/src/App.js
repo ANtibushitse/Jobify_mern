88a74dd71da3f15df9a85def255050107a26455d
ANtibushitse <ntibushitse.arsene@gmail.com>
2022-04-23 22:41:25 +0200

    Crating the Router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 
 import Landing from "./pages/Landing";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-export const App = () => {
+const App = () => {
+ /* A React Router. */
   return (
     <>
-
-      <Landing />
+    
+      <Router>
+        <Routes>
+          <Route path="/" element={<div>Dashbord</div>} />
+          <Route path="/register" element={<div>Register</div>} />
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/*" element={<div>Error</div>} />
+        </Routes>
+      </Router>
     </>
   )
 }
