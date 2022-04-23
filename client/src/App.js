6cc72fd1ec4263994888d34f33fe0a2a9cb7dc55
ANtibushitse <ntibushitse.arsene@gmail.com>
2022-04-23 19:04:05 +0200

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Landing from "./pages/Landing";
+
+export const App = () => {
+  return (
+    <>
+      <h1>Jobify</h1>
+      <Landing />
+    </>
+  )
+}
+
+export default App;
