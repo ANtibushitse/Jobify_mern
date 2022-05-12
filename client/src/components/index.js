fef55297538b956f2746d53c0ddb99b955c015ba
ANtibushitse <ntibushitse.arsene@gmail.com>
2022-05-12 23:18:25 +0200

    8 etq utilisateur je veux pouvoir me connecter (#9)
    * Formulaire d'inscription 1/2
    
    * docs(Git Ignore): adding  config  file
    
    Adding .idea  file  to the gitignore.
    
    * docs(Git Ignore): adding  config  file
    
    Adding .idea  file  to the gitignore.
    
    * v0.0.0
    
    * refactor(every .jsx): adding  visibility by indenting  it
    
    * feat(FormRow): name, email, password
    
    Created a FormRow Component that create a raw for each type rows (
    email,  password, etc etc)
    
    * feat(FormRow): name, email, password
    
    Created a FormRow Component that create a raw for each type rows (
    email,  password, etc etc)
    
    * feat(Register.js): Alert.js
    
    I have just created an Alert component which, when the form fields are
    completed or not completed, displays a message.
    
    * feat(Register): Register Component done.
    
    1-I have created a function that checks that the value of the isMember
    props is the opposite of what it currently is.
    2-If the value of
    isMember is False the Form is displayed.

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,11 @@
-
+import FormRow from './FormRow'
+import Alert from './Alert'
 import Logo from './Logo'
+
+
 
 export {
     Logo,
+    FormRow,
+    Alert
 }
--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import img from "../assets/images/not-found.svg";
 import Wrapper from "../assets/wrappers/ErrorPage";
 
@@ -6,7 +6,7 @@ const Error = () => {
     return (
         <Wrapper className="full-page">
             <div>
-                <img src={img} alt="not found" />
+                <img src={img} alt="not found"/>
                 <h3>text</h3>
                 <p>text</p>
                 <Link to="/">back home</Link>
