bba68553e82745de9d7514ccf5414033585938d1
ANtibushitse <ntibushitse.arsene@gmail.com>
2022-05-12 23:02:49 +0200

    feat(Register): Register Component done.
    1-I have created a function that checks that the value of the isMember
    props is the opposite of what it currently is.
    2-If the value of
    isMember is False the Form is displayed.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from "react";
-import {Logo, FormRow,Alert} from "../components";
+import {Logo, FormRow, Alert} from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
 // global context and useNavigate later
 /* Setting the initial state of the form. */
@@ -12,19 +12,17 @@ const initialState = {
     showAlert: false,
 };
 
-
 function Register() {
     const [values, setValues] = useState(initialState);
-
-
-
     /**
-     * A function that is called when the form is submitted.
+     * ToggleMember is a function that sets the value of isMember to the opposite of what it currently is
      */
+    const ToggleMember = () => {
+        setValues({...values, isMember: !values.isMember});
+    }
     const handleChange = (e) => {
         console.log(e.target);
     };
-
     /**
      * The function onSubmit is called when the form is submitted
      */
@@ -33,21 +31,44 @@ function Register() {
         console.log(e.target);
     };
     return (
-        /*  */
         <Wrapper className="full-page">
             <form className="form" onSubmit={onSubmit}>
                 <Logo/>
-                <h3>Login</h3>
-                {values.showAlert && (<Alert />)}
-                {/* name field */}
-                <FormRow type="text" name="name" label="Name" value={values.name} onChange={handleChange}/>
+                <h3>{values.isMember ? "Login" : "Register "}</h3>
+                {/**  A conditional rendering.
+                 * If the value of isMember is false,
+                 * then the FormRow component will be rendered.
+                 */}
+                {!values.isMember &&
+                (<FormRow type="text"
+                          name="name"
+                          label="Name"
+                          value={values.name}
+                          onChange={handleChange}/>)}
+                {values.showAlert && (<Alert/>)}
                 {/* email field */}
-                <FormRow type="email" name="email" label="email" value={values.email} onChange={handleChange}/>
+                <FormRow type="email"
+                         name="email"
+                         label="email"
+                         value={values.email}
+                         onChange={handleChange}/>
                 {/* password field */}
-                <FormRow type="password" name="password" label="password" value={values.password}onChange={handleChange}/>
+                <FormRow type="password"
+                         name="password"
+                         label="password"
+                         value={values.password}
+                         onChange={handleChange}/>
                 <button type="submit" className="btn btn-block">
-                    submit
+                    Submit
                 </button>
+                <p>
+                    {values.isMember ? "Not a member yet?" : "Already a member?"}
+                    <button type="button"
+                            className="member-btn"
+                            onClick={ToggleMember}>
+                        {values.isMember ? "Register" : "Login"}
+                    </button>
+                </p>
             </form>
         </Wrapper>
     );
