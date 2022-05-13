73688f28941ad0f68cfb24851edf320678eb9e66
ANtibushitse <ntibushitse.arsene@gmail.com>
2022-05-13 07:00:56 +0200

    feat(context folder): setting up  context

---
+++ client/context/appContex.js
@@ -0,0 +1,19 @@
+import {useState, useReducer, useContext} from 'react';
+
+export const initialState = {
+    isLoading: false,
+    showAlert: false,
+    alertText: '',
+    alertType: '',
+}
+const AppContext = React.createContext();
+const AppProvider = ({children}) => {
+    const [state, setState] = useState(initialState);
+    return (
+        <AppContext.Provider value={{...state}}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export {AppProvider,initialState}
