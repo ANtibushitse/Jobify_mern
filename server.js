e49ce519f073dcb7f18b6eaeb9e697b269dda646
arsene.ntibus <ntibushitse.arsene@gmail.com>
2022-05-20 14:53:34 +0200

    feat(Setting up error handler): error handler
    Setting Up Error handler.

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 import express from "express";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 const app = express();
 // middleware
 notFoundMiddleware
 app.get("/",(req,res )=>{
+    throw new Error("error");
     res.send("Hello World");
 },
 
 );
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware)
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
