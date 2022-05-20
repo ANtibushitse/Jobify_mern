57a8c7d1d0cc3e8faeda2957930c7051f38eaf1e
arsene.ntibus <ntibushitse.arsene@gmail.com>
2022-05-20 12:15:59 +0200

    Added useAppContext in order to display dynamically the alerts.

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -1,5 +1,8 @@
+import { useAppContext } from '../context/appContext';
+
 function Alert() {
-  return <div className="alert alert-danger"> Some Alert Here</div>;
+  const { alertType, alertText } = useAppContext();
+  return <div className={`alert alert-${alertType}`}>{alertText}</div>;
 }
 
 export default Alert;
