d10e9c76357794c786b4457689c755356576b512
ANtibushitse <ntibushitse.arsene@gmail.com>
2022-05-22 17:55:10 +0200

    Controllers and routes (#16)
    * feat(ControllerAndRoutes) Controller and routes initialisation.
    
    * Created some dummy routes
    
    * typo fix

---
+++ controllers/jobsController.js
@@ -0,0 +1,20 @@
+const createJob = async (req, res) => {
+    res.send('create job')
+}
+const getAllJobs = async (req, res) => {
+    res.send('Get all jobs')
+} 
+const deleteJob = async (req, res) => {
+    res.send('delete job')
+}
+const updateJob = async (req, res) => {
+    res.send(' job ipdated')
+}
+const showStats = async (req, res) => {
+    res.send('Show Stats')
+}
+
+
+
+export {createJob,getAllJobs,deleteJob,updateJob, showStats
+}
