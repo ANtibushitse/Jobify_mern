a20dcb20e512a590b5578e0216ba44cbea85bfdc
arsene.ntibus <ntibushitse.arsene@gmail.com>
2022-05-22 17:54:19 +0200

    Controller & Routes Done.

---
+++ routes/jobsRoutes.js
@@ -0,0 +1,18 @@
+// remember about :id
+import express from 'express'
+const router = express.Router()
+
+import {
+    createJob,
+    deleteJob,
+    getAllJobs,
+    updateJob,
+    showStats,
+} from '../controllers/jobsController.js'
+
+router.route('/').post(createJob).get(getAllJobs)
+// remember about :id
+router.route('/stats').get(showStats)
+router.route('/:id').delete(deleteJob).patch(updateJob)
+
+export default router
