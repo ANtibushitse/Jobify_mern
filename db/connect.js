ac41204f775fb89de1a17d246cdc55698eef6ccb
arsene.ntibus <ntibushitse.arsene@gmail.com>
2022-05-23 12:21:50 +0200

    refactor(*): Indentation
    Indented the code for better visibility.

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
 function connectDB(url) {
-	return mongoose.connect(url)
-		.then(() => console.log("CONNCETED TO THE DB"))
-		.catch(err => console.log(err));
+		return mongoose.connect(url)
+				.then(() => console.log("CONNCETED TO THE DB"))
+				.catch(err => console.log(err));
 }
 
 export default connectDB;
