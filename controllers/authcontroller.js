f201c32c254dd0b08e50dd6ba951f70438ccb2ab
arsene.ntibus <ntibushitse.arsene@gmail.com>
2022-05-23 09:19:37 +0200

    feat(Passing error  to the ErrorMidleWare):
    Passing the Error to the Error handler with the next(error).
    
    BREAKING CHANGE: no breaking  changes.

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -2,19 +2,20 @@
 import User from "../Models/User.js";
 
 
+
 /**
- * It creates a new user in the database using the information provided in the request body
- * 
- * @param req The request object represents the HTTP request and has properties for the request query
- * string, parameters, body, HTTP headers, and so on.
- * @param res The response object.
+ * It creates a new user in the database using the data from the request body
+ * @param req - The request object represents the HTTP request and has properties for the request query string, parameters,
+ * body, HTTP headers, and so on.
+ * @param res - The response object.
+ * @param next - This is a function that we call when we want to move on to the next middleware.
  */
-const register= async (req, res) => {
+const register= async (req, res, next) => {
     try {
         const user = await User.create(req.body);
         res.status(201).json(user);
     } catch (error) {
-        res.status(500).json({msg: 'There was an error Registering the user'});
+       next(error);
     }
 
 }
