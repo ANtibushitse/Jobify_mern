809f8bd0cb19ffbe31ca4b768cb3c813b07c0dc8
arsene.ntibus <ntibushitse.arsene@gmail.com>
2022-05-23 15:14:05 +0200

    Fix of the ErrorHanling API

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,18 +1,12 @@
 import User from "../Models/User.js";
 import { StatusCodes } from 'http-status-codes';
+import { BadRequestError } from '../errors/index.js';
 
-/**
- * It creates a new user in the database using the data from the request body
- * @param req - The request object represents the HTTP request and has properties for the request query string, parameters,
- * body, HTTP headers, and so on.
- * @param res - The response object.
- * @param next - This is a function that we call when we want to move on to the next middleware.
- */
+
 const register= async (req, res) => {
 		const { name, email, password, } = req.body;
-
 		if(!name || !email || !password) {
-				throw new Error("Please provide all the required fields");
+				throw new BadRequestError( "Please provide all the required fields");
 		}
 		const user = await User.create(name, email, password);
 		res.status(StatusCodes.created).json({user});
