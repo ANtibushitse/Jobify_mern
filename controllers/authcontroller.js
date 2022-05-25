f96f7300456f27d75b11f615813690934532f511
arsene.ntibus <ntibushitse.arsene@gmail.com>
2022-05-25 14:58:26 +0200

    fix:AuthController Fixed

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -5,26 +5,24 @@ import { BadRequestError } from '../errors/index.js';
 
 
 const register= async (req, res) => {
-		const { name, email, password, } = req.body;
+	const { name, email, password } = req.body
 
-		if(!name || !email || !password) {
-				throw new BadRequestError( "Please provide all the required fields");
-		}
-		/* Checking if the user already exists in the database. */
-		const userAlreadyExists = await User.findOne({ email });
-		if(userAlreadyExists) {
-				throw new BadRequestError( "User already exists");
-		}
-		/* Creating a new user and returning the user object. */
-		const user = await User.create(name, email, password);
-		res.status(StatusCodes.OK).json({user});
+	if (!name || !email || !password) {
+		throw new BadRequestError('please provide all values')
+	}
+	const userAlreadyExists = await User.findOne({ email })
+	if (userAlreadyExists) {
+		throw new BadRequestError('Email already in use')
+	}
+	const user = await User.create({ name, email, password })
+	res.status(StatusCodes.OK).json({user});
 		if(!name || !email || !password) {
 				throw new BadRequestError( "Please provide all the required fields");
 		}
 }
 
-const login= async(req, res) => {
-		res.send('login user');
+async function login(req, res) {
+	res.send('login user');
 }
 const updateUser= async (req, res) => {
 		res.send(' user Updted');
