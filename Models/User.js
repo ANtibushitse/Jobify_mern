e0ada46b8c82a529910cf5f0b9ad8ab8e1a8f060
arsene.ntibus <ntibushitse.arsene@gmail.com>
2022-05-25 14:59:47 +0200

    Reviwed the schema

--- Models/User.js
+++ Models/User.js
@@ -1,48 +1,43 @@
 import mongoose from "mongoose";
 import validator from "validator";
-import bcrypt from 'bcryptjs';
+
 /* Creating a schema for the user model. */
 const UserSchema = new mongoose.Schema({
-		name: {
-				type: String,
-				required: [true, "Name is required"],
-				minlength: [3, "Name must be at least 3 characters long"],
-				maxlength: [20, "Name must be less than 20 characters long"],
-				trim: true,
+	name: {
+		type: String,
+		required: [true, 'Please provide name'],
+		minlength: 3,
+		maxlength: 20,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: [true, "Email is required"],
+		validate: {
+			validator: validator.isEmail,
+			message: "please add a valid email"
 		},
-		email: {
-				type: String,
-				required: [true, "Email is required"],
-				validate: {
-						validator: validator.isEmail,
-						message: "please add a valid email"
-				},
-				unique: true,
-		},
-		password: {
-				type: String,
-				required: [true, "Password is required"],
-				minlength: [8, "Password must be at least 8 characters long"],
-		},
-		lastName: {
-				type: String,
-				trim: true,
-				maxlength: 20,
-				default: "Last Name",
-		},
-		location: {
-				type: String,
-				trim: true,
-				maxlength: 20,
-				default: "My City"
-		},
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: [true, "Password is required"],
+		minlength: [8, "Password must be at least 8 characters long"],
+	},
+	lastName: {
+		type: String,
+		trim: true,
+		maxlength: 20,
+		default: "Last Name",
+	},
+	location: {
+		type: String,
+		trim: true,
+		maxlength: 20,
+		default: "My City"
+	},
 });
 /* This is a middleware that is run before the user is saved to the database. It checks if the password
 has been modified and if it has, it hashes the password. */
-UserSchema.pre("save", async function (next) {
-		if (!this.isModified('password')) return
-		const salt = await bcrypt.genSalt(10)
-		this.password = await bcrypt.hash(this.password, salt)
-});
 
 export default mongoose.model("User", UserSchema);
