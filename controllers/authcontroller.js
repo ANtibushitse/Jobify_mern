d30336d7ffeb968542d95542dbf6a5b0945e5629
ANtibushitse <ntibushitse.arsene@gmail.com>
2022-05-25 16:08:00 +0200

    Jwt (#23)
    * fix:AuthController Fixed
    
    * Reviwed the schema
    
    * Corrrected  the status sent
    
    * Everytime that i create an user, a JWT Token is created as well.
    
    * implement the methode to creat a JWT token.
    
    * JWT added to tthe package.json
    
    * JWT Done

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -5,26 +5,36 @@ import { BadRequestError } from '../errors/index.js';
 
 
 const register= async (req, res) => {
-		const { name, email, password, } = req.body;
+	const { name, email, password } = req.body
 
-		if(!name || !email || !password) {
-				throw new BadRequestError( "Please provide all the required fields");
-		}
-		/* Checking if the user already exists in the database. */
-		const userAlreadyExists = await User.findOne({ email });
-		if(userAlreadyExists) {
-				throw new BadRequestError( "User already exists");
-		}
-		/* Creating a new user and returning the user object. */
-		const user = await User.create(name, email, password);
-		res.status(StatusCodes.OK).json({user});
+	if (!name || !email || !password) {
+		throw new BadRequestError('please provide all values')
+	}
+	const userAlreadyExists = await User.findOne({ email })
+	if (userAlreadyExists) {
+		throw new BadRequestError('Email already in use')
+	}
+	const user = await User.create({ name, email, password })
+	const token = user.createJWT()
+	res.status(StatusCodes.CREATED).json({
+		/* Creating a new user object and assigning it to the user variable. */
+		user: {
+			email: user.email,
+			lastName: user.lastName,
+			location: user.location,
+			name: user.name,
+		},
+		token,
+		location: user.location,
+	})
+
 		if(!name || !email || !password) {
 				throw new BadRequestError( "Please provide all the required fields");
 		}
 }
 
-const login= async(req, res) => {
-		res.send('login user');
+async function login(req, res) {
+	res.send('login user');
 }
 const updateUser= async (req, res) => {
 		res.send(' user Updted');
