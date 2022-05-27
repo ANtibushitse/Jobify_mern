7e64931e5abdaa9286a0d7588616deb2a83a9cb2
arsene.ntibus <ntibushitse.arsene@gmail.com>
2022-05-27 14:03:39 +0200

    Organising  the connexion between frontend and backend

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -8,23 +8,23 @@ const register= async (req, res) => {
 		const { name, email, password, } = req.body;
 
 		if(!name || !email || !password) {
-				throw new BadRequestError( "Please provide all the required fields");
+				throw new BadRequestError( "Veuillez Remplir tous les champs");
 		}
 		/* Checking if the user already exists in the database. */
 		const userAlreadyExists = await User.findOne({ email });
 		if(userAlreadyExists) {
-				throw new BadRequestError( "User already exists");
+				throw new BadRequestError( "l'utilisateur existe déjà");
 		}
 		/* Creating a new user and returning the user object. */
 		const user = await User.create(name, email, password);
 		res.status(StatusCodes.OK).json({user});
 		if(!name || !email || !password) {
-				throw new BadRequestError( "Please provide all the required fields");
+				throw new BadRequestError( "Veuillez Remplir tous les champs");
 		}
 }
 
 const login= async(req, res) => {
-		res.send('login user');
+		res.send(' user logged in');
 }
 const updateUser= async (req, res) => {
 		res.send(' user Updted');
