4dd937046a34025a4560e1f0f0b316e3f87e8c2f
Antibushitse <arsene@Arsene>
2023-02-05 13:04:53 +0100

    switched to react

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
 import App from './App';
 import { AppProvider } from './context/appContext';
 
-const root = React.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppProvider>
       <App />
     </AppProvider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
